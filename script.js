0a72aab7b3883c8715821f23f2e0db0b26305e56
dmytropogribnyy <dipptrue@gmail.com>
2020-06-14 20:52:03 +0300

    Chill-out app created

---
+++ script.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("container");
+const text = document.getElementById("text");
+const totalTime = 8000;
+
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+// console.log(breathTime, holdTime);
+
+breathAnimation();
+
+function breathAnimation() {
+	text.innerText = "Breathe In";
+	container.className = "container grow";
+
+	setTimeout(() => {
+		text.innerText = "Hold";
+		setTimeout(() => {
+			text.innerText = "Breathe Out";
+			container.className = "container shrink";
+		}, holdTime);
+	}, breathTime);
+}
+
+setInterval(breathAnimation, totalTime);
