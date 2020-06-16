7513899608d41ac8386d0ecc9b6d160b31dc614e
dmytropogribnyy <dipptrue@gmail.com>
2020-06-16 19:01:02 +0300

    updated

--- script.js
+++ script.js
@@ -2,22 +2,26 @@ const container = document.getElementById("container");
 const pointer_container = document.getElementById("pointer-container");
 const text = document.getElementById("text");
 const totalTime = 8000;
-const startbtn = document.getElementsById("startbtn");
-const stopbtn = document.getElementById("stopbtn");
+const startbtn = document.querySelector("startbtn");
+const stopbtn = document.querySelector("stopbtn");
 
 const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
+// breathAnimation();
+// setInterval(breathAnimation, totalTime);
+pointer_container.style.animationPlayState = "paused";
+
 function animationStart() {
 	pointer_container.style.animationPlayState = "running";
-	breathAnimation();
 }
 
 function animationStop() {
 	pointer_container.style.animationPlayState = "paused";
 }
 
-// breathAnimation();
+startbtn.addEventListener("click", animationStart);
+stopbtn.addEventListener("click", animationStop);
 
 function breathAnimation() {
 	text.innerText = "Breathe In";
