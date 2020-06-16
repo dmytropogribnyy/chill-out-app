eff707f85a9bca0761aa58a8a35f15d12cb5ec89
dmytropogribnyy <dipptrue@gmail.com>
2020-06-16 12:16:34 +0300

    Added Feature

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const container = document.getElementById("container");
+const pointer_container = document.getElementById("pointer-container");
 const text = document.getElementById("text");
 const totalTime = 8000;
+const startbtn = document.getElementsById("startbtn");
+const stopbtn = document.getElementById("stopbtn");
 
 const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
-// console.log(breathTime, holdTime);
+function animationStart() {
+	pointer_container.style.animationPlayState = "running";
+	breathAnimation();
+}
 
-breathAnimation();
+function animationStop() {
+	pointer_container.style.animationPlayState = "paused";
+}
+
+// breathAnimation();
 
 function breathAnimation() {
 	text.innerText = "Breathe In";
