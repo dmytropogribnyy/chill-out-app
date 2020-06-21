28eaab152dc1ef4601e8dcf27fe733c872ce4445
dmytropogribnyy <dipptrue@gmail.com>
2020-06-21 12:38:44 +0300

    Updated feature

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # chill-out-app
+
 The breathing app for relaxation
+
+## Project Specifications
+
+- Create circle and gradient circle with CSS
+- Create and animate pointer (Small circle)
+- Create grow and shrink animations
+- Add JavaScript to create the breath animation effect
--- script.js
+++ script.js
@@ -2,48 +2,52 @@ const container = document.getElementById("container");
 const pointerContainer = document.getElementById("pointer-container");
 const text = document.getElementById("text");
 const totalTime = 8000;
-const startbtn = document.querySelector("#startbtn");
-const stopbtn = document.querySelector("#stopbtn");
+const startbtn = document.querySelector("startbtn");
+const stopbtn = document.querySelector("stopbtn");
 
 const breathTime = (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
-
-let intervalId, timeoutId;
-
-intervalId = setInterval(breathAnimation, totalTime);
-
-timeoutId = setTimeout(breathAnimation, totalTime);
 
 pointerContainer.style.animationPlayState = "paused";
 
 startbtn.addEventListener("click", animationStart);
 stopbtn.addEventListener("click", animationStop);
 
+var breath;
+var hold_timeout;
+var breath_out;
+
 function animationStart() {
-	// pointerContainer.style.animationPlayState = "running";
-	container.style.animation = "";
-	pointerContainer.style.animation = "";
 	breathAnimation();
-	intervalId();
-	timeoutId();
+	breath = setInterval(breathAnimation, totalTime);
+
+	setTimeout(() => {
+		pointerContainer.style.animation = "";
+	}, 150);
 }
 
 function animationStop() {
-	container.style.animation = "none";
 	pointerContainer.style.animation = "none";
-	clearInterval(intervalId);
-	clearTimeout(timeoutId);
+	container.className = "container";
+	clearInterval(breath);
+	clearTimeout(hold_timeout);
+	clearTimeout(breath_out);
+	text.innerText = "";
 }
 
 function breathAnimation() {
-	text.innerText = "Breathe In";
-	container.className = "container grow";
+	if (animationStart) {
+		text.innerText = "Breathe In";
+		container.className = "container grow";
 
-	setTimeout(() => {
-		text.innerText = "Hold";
-		setTimeout(() => {
-			text.innerText = "Breathe Out";
-			container.className = "container shrink";
-		}, holdTime);
-	}, breathTime);
+		hold_timeout = setTimeout(() => {
+			text.innerText = "Hold";
+			breath_out = setTimeout(() => {
+				text.innerText = "Breathe Out";
+				container.className = "container shrink";
+			}, holdTime);
+		}, breathTime);
+	} else if (animationStop) {
+		return;
+	}
 }
